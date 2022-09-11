bb2c473836d37d6e95e29fb9bbd2e88c5754f5ee
Nathan Luong <nathan@jamlabs.com>
2022-09-11 19:43:48 -0400

    Got some data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,61 @@
+import { data } from "autoprefixer";
+import { useEffect, useState, useRef } from "react";
+
 function App() {
-  return <div></div>;
+  const [audio, setAudio] = useState<HTMLAudioElement>(
+    new Audio("/audio/C_chord.mp3")
+  );
+  const [isPaused, setPaused] = useState<boolean>(true);
+  const [ctx, setCtx] = useState<AudioContext>();
+  const [dataArray, setDataArray] = useState<Uint8Array>();
+  const analyser = useRef<AnalyserNode>();
+  const source = useRef<MediaElementAudioSourceNode>();
+
+  useEffect(() => {
+    const update = () => {
+      requestAnimationFrame(update);
+      if (analyser.current !== undefined && dataArray !== undefined) {
+        console.log(dataArray);
+        analyser.current.getByteFrequencyData(dataArray);
+      }
+    };
+    update();
+  }, [dataArray]);
+
+  useEffect(() => {
+    audio.load();
+    //@ts-ignore
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  }, [audio]);
+
+  useEffect(() => {
+    if (ctx !== undefined) {
+      analyser.current = ctx.createAnalyser();
+      source.current = ctx.createMediaElementSource(audio);
+      source.current.connect(analyser.current);
+      source.current.connect(ctx.destination);
+      analyser.current.fftSize = 64;
+      const bufferLength = analyser.current.frequencyBinCount;
+      setDataArray(new Uint8Array(bufferLength));
+    }
+  }, [ctx, audio]);
+
+  const toggleSound = () => {
+    isPaused ? setPaused(false) : setPaused(true);
+    audio.paused ? audio.play() : audio.pause();
+  };
+
+  const actiavteContext = () => {
+    ctx === undefined ? setCtx(new window.AudioContext()) : void 0;
+  };
+
+  return (
+    <main className="w-full h-full" onClick={actiavteContext}>
+      <div className="w-[80%] h-full flex flex-col gap-4">
+        <button onClick={toggleSound}>{isPaused ? `Play` : `Pause`}</button>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,7 @@ import "./styles/tailwind.css";
 import App from "./App";
 import React from "react";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
